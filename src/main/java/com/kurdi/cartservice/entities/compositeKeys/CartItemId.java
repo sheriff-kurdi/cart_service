2160cd0c22ee2184b3cf919a368d8e3c90b91070
Kurdi <sheriff.kurdi@gmail.com>
2022-03-30 14:14:53 +0200

    refactoring

--- src/main/java/com/kurdi/cartservice/entities/compositeKeys/CartItemId.java
+++ src/main/java/com/kurdi/cartservice/entities/compositeKeys/CartItemId.java
@@ -1,23 +1,18 @@
 package com.kurdi.cartservice.entities.compositeKeys;
 
+
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public final class CartItemId implements Serializable {
-    // CartItem can be repeated(so if it repeated add the quantty rather than create new cart item)
+    // CartItem can be repeated(so if it repeated add the quantity rather than create new cart item)
      Integer identity;
      String SKU;
 
-    public CartItemId() {
-    }
-
-    public CartItemId(Integer identity, String SKU) {
-        this.identity = identity;
-        this.SKU = SKU;
-    }
 }
