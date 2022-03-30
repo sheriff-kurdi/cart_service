96b786314469195ec0f8301f8a33ec7bd7b9c57c
Kurdi <sheriff.kurdi@gmail.com>
2022-03-30 11:13:12 +0200

    addToCart

--- src/main/java/com/kurdi/cartservice/controllers/CartsController.java
+++ src/main/java/com/kurdi/cartservice/controllers/CartsController.java
@@ -1,13 +1,12 @@
 package com.kurdi.cartservice.controllers;
 
 import com.kurdi.cartservice.entities.Cart;
+import com.kurdi.cartservice.entities.CartItem;
 import com.kurdi.cartservice.repositories.CartsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SuppressWarnings("unused")
 @RestController
@@ -17,14 +16,31 @@ public class CartsController {
     @Autowired
     CartsRepository cartsRepository;
 
-    @GetMapping("{identity}")
-    public ResponseEntity<Cart> getCart(Integer identity)
+    @GetMapping()
+    public ResponseEntity<Cart> getCart()
     {
+        Integer identity = 0;//TODO: get identity from token;
         if(cartsRepository.existsById(identity))
         {
             Cart cart = cartsRepository.getById(identity);
             return new ResponseEntity<>(cart, HttpStatus.OK);
         }
         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Cart> addToCart(@RequestBody CartItem cartItem)
+    {
+        Integer identity = 0;//TODO: get identity from token;
+        if(!cartsRepository.existsById(identity))
+        {
+            Cart newCart = Cart.builder().Identity(identity).build();
+            cartsRepository.save(newCart);
+        }
+        Cart cart = cartsRepository.getById(identity);
+        cart.getItems().add(cartItem);
+        cartsRepository.save(cart);
+        return new ResponseEntity<>(cart, HttpStatus.OK);
+
     }
 }
