7315d857e988844b5068c02f4d802dd02a7bf3a9
Kurdi <sheriff.kurdi@gmail.com>
2022-03-30 10:50:24 +0200

    get cart

--- src/main/java/com/kurdi/cartservice/controllers/CartsController.java
+++ src/main/java/com/kurdi/cartservice/controllers/CartsController.java
@@ -1,8 +1,30 @@
 package com.kurdi.cartservice.controllers;
 
+import com.kurdi.cartservice.entities.Cart;
+import com.kurdi.cartservice.repositories.CartsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SuppressWarnings("unused")
 @RestController
+@RequestMapping("/carts/")
 public class CartsController {
+
+    @Autowired
+    CartsRepository cartsRepository;
+
+    @GetMapping("{identity}")
+    public ResponseEntity<Cart> getCart(Integer identity)
+    {
+        if(cartsRepository.existsById(identity))
+        {
+            Cart cart = cartsRepository.getById(identity);
+            return new ResponseEntity<>(cart, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
 }
